80ff401c1aa7dd8e714d7d0ef80dc7a24f2dc5cf
father-lox <kot.mi.mi.992@gmail.com>
2023-02-25 21:42:02 +0300

    Add load logic to comment model

--- components/models/model-comment.ts
+++ components/models/model-comment.ts
@@ -4,4 +4,39 @@ export default class ModelComment {
         readonly comment: string,
         readonly isAuthor: boolean
     ) {}
+
+    static requestAddress: string = 'http://localhost/api/comments';
+
+    static async getComments(newsId: number): Promise<ModelComment[]> {
+        const unserializedComments: Array<any> = await this.loadInOrder(newsId);
+        return this.serializeMany(unserializedComments);
+    }
+
+    static async loadInOrder(newsId: number): Promise<Array<Object>> {
+        return fetch(`${this.requestAddress}/${newsId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+        }).then(response => {
+            return response.json();
+        }).then(({data: comments, links}) => {
+            this.requestAddress = links.next;
+            return comments;
+        });
+    }
+
+    static serializeOne(
+        nickname: string,
+        comment: string,
+        isAuthor: boolean
+    ): ModelComment {
+        return new ModelComment(nickname, comment, isAuthor);
+    }
+
+    static serializeMany(unserializedComments: Array<any>): ModelComment[] {
+        return unserializedComments.map(comment => {
+            return this.serializeOne(comment.nickname, comment.comment, false)
+        }) as ModelComment[];
+    }
 }
