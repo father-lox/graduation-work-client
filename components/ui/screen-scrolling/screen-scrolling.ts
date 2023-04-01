ebc150f4180afe43dc1b5497f5d13fe419ab9dab
father-lox <kot.mi.mi.992@gmail.com>
2023-04-01 17:57:16 +0300

    Move screen-scrolling logic to independet file and add switch animation

---
+++ components/ui/screen-scrolling/screen-scrolling.ts
@@ -0,0 +1,128 @@
+export default class ScreenScrolling {
+    constructor(
+        private containerScreens: HTMLElement,
+        private screens: HTMLElement[],
+        private loadScreens?: () => Promise<HTMLElement[]>
+    ) {
+        if (screens.length === 0 && loadScreens) {
+            this.attachLoadedScreens().then(() => {
+                this.screens[this.currentScreen].classList.remove(this.classes.nextScreen);
+            });
+        } else if (screens.length === 0 && !loadScreens) {
+            throw new Error('The array of screens is empty');
+        }
+
+        this.containerScreens.classList.add(this.classes.container);
+        this.initKeydownEventOnContainerScreens();
+        this.initWheelEventOnContainerScreens();
+        this.containerScreens.addEventListener('transitionend', this.activateSwitching);
+    }
+
+    //TODO: При слабом скроле смещать новость, выполнять поготовку к анимации
+    //TODO: Если скрол был не достаточно сильный, обнулять switchAccumulator
+    //TODO: Реализовать поддержку touchscreens
+
+    private readonly nextSwitchThreshold: number = 250;
+    private readonly previousSwitchThreshold: number = -250;
+    private switchAccumulator: number = 0;
+    private isSwitchable: boolean = true;
+
+    private currentScreen: number = 0;
+    private classes: {
+        container: string,
+        screens: string,
+        nextScreen: string,
+        previousScreen: string
+    } = {
+        container: 'screen-scrolling',
+        screens: 'screen-scrolling__item',
+        nextScreen: 'screen-scrolling_next',
+        previousScreen: 'screen-scrolling_previous'
+    }
+
+    private canNextSwitch = (): boolean => {
+        return this.switchAccumulator >= this.nextSwitchThreshold && this.isSwitchable;
+    }
+
+    private canPreviousSwitch = (): boolean => {
+        return this.switchAccumulator <= this.previousSwitchThreshold && this.isSwitchable;
+    }
+
+    private blockSwitching = () => {
+        this.isSwitchable = false;
+    }
+
+    private activateSwitching = (event: Event) => {
+        if (event.target === this.screens[this.currentScreen]) {
+            this.isSwitchable = true;
+            this.resetSwitchAccumulator();
+        }
+    }
+
+    private resetSwitchAccumulator = () => {
+        this.switchAccumulator = 0;
+    }
+
+    private initWheelEventOnContainerScreens() {
+        this.containerScreens.addEventListener('wheel', event => {
+            this.switchAccumulator += event.deltaY;
+
+            if (this.canNextSwitch()) {
+                this.switchNext();
+            }
+            else if (this.canPreviousSwitch()) {
+                this.switchPrevious();
+            }
+        });
+    }
+
+    private initKeydownEventOnContainerScreens() {
+        window.addEventListener('keydown', (event) => {
+            if (this.isSwitchable && event.code === 'ArrowDown' && !event.repeat) {
+                this.switchNext();
+            }
+            else if (this.isSwitchable && event.code === 'ArrowUp' && !event.repeat) {
+                this.switchPrevious();
+            }
+        });
+    }
+
+    private switchNext = () => {
+        if (window.pageYOffset !== window.scrollY) {
+            return;
+        }
+    
+        if (this.currentScreen + 1 < this.screens.length) {
+            this.blockSwitching();    
+            this.screens[this.currentScreen].classList.add(this.classes.previousScreen);
+            this.currentScreen++;   
+            this.screens[this.currentScreen].classList.remove(this.classes.nextScreen);
+        }
+        if (this.currentScreen === this.screens.length - 1 && this.loadScreens) {
+            this.attachLoadedScreens();
+        }
+    
+        window.scroll(0, 0);
+    }
+
+    private switchPrevious = () => {
+        if (this.currentScreen > 0 && window.pageYOffset === 0) {
+            this.blockSwitching();
+            this.screens[this.currentScreen].classList.add(this.classes.nextScreen);
+            this.currentScreen--;
+            this.screens[this.currentScreen].classList.remove(this.classes.previousScreen);
+        } else {
+            this.resetSwitchAccumulator();
+        }
+    }
+
+    private attachLoadedScreens = async () => {
+        if (this.loadScreens) {
+            (await this.loadScreens()).forEach(element => {
+                element.classList.add(this.classes.nextScreen, this.classes.screens);
+                this.containerScreens.append(element);
+                this.screens.push(element);
+            });
+        }
+    }
+}
--- pages/index/index.ts
+++ pages/index/index.ts
@@ -1,5 +1,6 @@
 import ModelNews from "../../components/models/model-news.js";
 import HTMLNews from "../../components/ui/news/html-news.js";
+import ScreenScrolling from "../../components/ui/screen-scrolling/screen-scrolling.js";
 import RenderNews from '../../components/renders/render-news.js'
 import CommentsManger from './portions/comments/comments-manager.js';
 
@@ -7,68 +8,18 @@ new CommentsManger().init();
 
 const rendererNews: RenderNews = new RenderNews();
 const newsContainer: HTMLDivElement = document.querySelector('.news-section') as HTMLDivElement;
-const nodesNews: HTMLNews[] = [];
+const newsElements: HTMLNews[] = [];
 let currentNews: number = 0;
 
 if (!newsContainer) {
     throw new Error(".news-section is undefined");
 }
 
-updateNewsFeed().then(() => {nodesNews[currentNews].style.display = 'block'});
 
+new ScreenScrolling(newsContainer, newsElements, loadNews);
 
-window.addEventListener('keydown', (event) => {
-    if (event.code === 'ArrowDown' && !event.repeat) {
-        switchToNextNews();
-    }
-    else if (event.code === 'ArrowUp' && !event.repeat) {
-        switchToPreviousNews();
-    }
-});
-
-newsContainer.addEventListener('wheel', event => {
-    if (event.deltaY > 0) {
-        switchToNextNews();
-    }
-    else if (event.deltaY < 0) {
-        switchToPreviousNews();
-    }
-});
 
 async function loadNews(): Promise<HTMLNews[]> {
     const modelsNews: ModelNews[] = await ModelNews.getNews();
     return rendererNews.renderMany(modelsNews) as HTMLNews[];
-}
-
-async function updateNewsFeed() {
-    (await loadNews()).forEach(node => {
-        node.style.display = 'none'
-        newsContainer.append(node);
-        nodesNews.push(node);
-    });
-}
-
-function switchToNextNews() {
-    if (window.pageYOffset !== window.scrollY) {
-        return;
-    }
-
-    if (currentNews + 1 < nodesNews.length) {
-        nodesNews[currentNews].style.display = 'none';
-        currentNews++;   
-        nodesNews[currentNews].style.display = 'block';
-    }
-    if (currentNews === nodesNews.length - 1) {
-        updateNewsFeed();
-    }
-
-    window.scroll(0, 0);
-}
-
-function switchToPreviousNews() {
-    if (currentNews > 0 && window.pageYOffset === 0) {
-        nodesNews[currentNews].style.display = 'none';
-        currentNews--;
-        nodesNews[currentNews].style.display = 'block';
-    }
 }
